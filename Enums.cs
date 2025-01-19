49cb2140cf4ccc697c2fc3b66d2611ed22b4c768
Radosav Panic <radosav.panic01@gmail.com>
2025-01-19 14:11:54 +0100

    Add project files.

---
+++ Enums.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Linq;
+
+namespace CSLab
+{
+    internal class Enums
+    {
+        private enum EnumDays { Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday}
+
+        private enum EnumTemperatures
+        {
+            NeutralZero = -18,
+            FreezingPoint = 0,
+            LightJacketWeather = 16,
+            SwimmingWeather = 22,
+            BoilingPoint = 100
+        }
+
+        public static string[] GetDaysArray()
+        {
+        string[] daysOfWeek = new string[Enum.GetValues(typeof(EnumDays)).Length];
+        int daysIndex = 0;
+
+        foreach (EnumDays day in Enum.GetValues(typeof(EnumDays))) daysOfWeek[daysIndex++] = day.ToString();
+
+        return daysOfWeek;
+        }
+           
+        public static string DisplayAllDays(string[] daysOfWeek)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (string day in daysOfWeek)
+            {
+                sb.Append(day);
+                if (day != daysOfWeek[daysOfWeek.Length - 1]) sb.Append(", ");
+            }
+
+            return sb.ToString(); // Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+        }
+
+        public static string DisplayAllTemperatures()
+        {
+            Dictionary<string, int> tempDict = new Dictionary<string, int>();
+
+            foreach (EnumTemperatures temp in Enum.GetValues(typeof(EnumTemperatures))) tempDict.Add(temp.ToString(), (int)temp);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach(var entry in tempDict)
+            {
+                sb.Append($"{entry.Key}: {entry.Value}");
+                if(entry.Key != tempDict.Last().Key)
+                {
+                    sb.Append(", ");
+                }
+            }
+
+            return sb.ToString(); // FreezingPoint: 0, LightJacketWeather: 16, SwimmingWeather: 22, BoilingPoint: 100, NeutralZero: -18
+        }                    
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using EnumsSpace = CSLab.Enums;
+
+namespace CSLab
+{
+    class CSLab
+    {
+        static void Main(string[] args)
+        {
+            string[] daysOfWeek = EnumsSpace.GetDaysArray();
+            Console.WriteLine(EnumsSpace.DisplayAllDays(daysOfWeek));            
+
+            Console.WriteLine(EnumsSpace.DisplayAllTemperatures());
+        }
+    }
+}
