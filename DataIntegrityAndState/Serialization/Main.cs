db4d5c85ee23aaf82b586da4d6c55bc1622b4931
Radosav Panic <radosav.panic01@gmail.com>
2025-01-21 15:27:17 +0100

    implemented methods for serialization/deserialization in json format

--- DataIntegrityAndState/Serialization/Main.cs
+++ DataIntegrityAndState/Serialization/Main.cs
@@ -1,35 +1,15 @@
 ﻿using System;
 using System.IO;
 using System.Text.Json;
-
+using static CSLab.DataIntegrityAndState.Serialization.Purchase;
 
 namespace CSLab.DataIntegrityAndState.Serialization
 {
     public class Main
-    {
+    {       
         public static void Run()
         {
-            var purchase1 = new Purchase()
-            {
-                ProductName = "Liquid Soap",
-                DateTime  = DateTime.UtcNow,
-                ProductPrice = 2.49m
-            };
-
-            var serializerOptions = new JsonSerializerOptions
-            {
-                PropertyNameCaseInsensitive = true,
-                WriteIndented = true,
-            };
-
-            
-            string jsonString = JsonSerializer.Serialize<Purchase>(purchase1, serializerOptions);
-            byte[] jsonByte = JsonSerializer.SerializeToUtf8Bytes(purchase1);
-
-            string projectRoot = AppContext.BaseDirectory;
-            string filePath = Path.Combine(projectRoot, "..", "..", "..", "DataIntegrityAndState", "Serialization", "purchase.json");
-
-            File.WriteAllText(filePath, jsonString);
+            DisplayPurchases();
         }
     }
 }
--- DataIntegrityAndState/Serialization/Purchase.cs
+++ DataIntegrityAndState/Serialization/Purchase.cs
@@ -1,15 +1,104 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Text.Json;
 
 namespace CSLab.DataIntegrityAndState.Serialization
-{
-    public class Purchase
     {
-        public string? ProductName { get; set; }
-        public DateTime DateTime { get; set; }
-        public decimal ProductPrice { get; set; }
+        public class Purchase
+        {
+            public string? ProductName { get; set; }
+            public DateTime DateTime { get; set; }
+            public decimal ProductPrice { get; set; }
+
+            private static string projectRoot = AppContext.BaseDirectory;
+            private static string filePath = Path.Combine(projectRoot, "..", "..", "..", "DataIntegrityAndState", "Serialization", "purchase.json");
+
+            public static void MakeNewPurchase(string productName, decimal price)
+            {
+                var purchase = new Purchase()
+                {
+                    ProductName = productName,
+                    DateTime = DateTime.UtcNow,
+                    ProductPrice = price
+                };
+
+                var serializerOptions = new JsonSerializerOptions
+                {
+                    PropertyNameCaseInsensitive = true,
+                    WriteIndented = true,
+                };
+
+                List<Purchase> purchaseList;
+
+                try
+                {
+
+                    if (File.Exists(filePath))
+                    {
+                        string existingJson = File.ReadAllText(filePath);
+
+                        purchaseList = string.IsNullOrWhiteSpace(existingJson) ? new List<Purchase>() : JsonSerializer.Deserialize<List<Purchase>>(existingJson, serializerOptions);
+                    }
+                    else
+                    {
+                        purchaseList = new List<Purchase>();
+                    }
+
+                    purchaseList.Add(purchase);
+
+                    string jsonString = JsonSerializer.Serialize(purchaseList, serializerOptions);
+
+                    File.WriteAllText(filePath, jsonString);
+                    Console.WriteLine("Purchase successful!");
+                }
+                catch (IOException ex) when (ex is FileNotFoundException or DirectoryNotFoundException)
+                {
+                    Console.WriteLine($"File error: {ex.Message}");
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine($"Json serialization error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An unexpected error occured: {ex.Message}");
+                }
+
+            }
+
+            public static void DisplayPurchases()
+            {
+                try
+                {                    
+                    var serializerOptions = new JsonSerializerOptions
+                    {
+                        PropertyNameCaseInsensitive = true,
+                        WriteIndented = true,
+                    };
+
+                
+                    string existingJson = File.ReadAllText(filePath);
+                    var purchaseList = JsonSerializer.Deserialize<List<Purchase>>(existingJson, serializerOptions);
+
+                    Console.WriteLine("Products List");
+                    foreach (var purchase in purchaseList)
+                    {                        
+                        Console.WriteLine($"Product: {purchase.ProductName}, price: {purchase.ProductPrice}, date: {purchase.DateTime}");
+                    }
+                }
+                catch (IOException ex) when (ex is FileNotFoundException or DirectoryNotFoundException)
+                {
+                    Console.WriteLine($"File error: {ex.Message}");
+                }
+                catch (JsonException ex)
+                {
+                    Console.WriteLine($"Json serialization error: {ex.Message}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"An unexpected error occured: {ex.Message}");
+                }
+            }
+
+        }
     }
-}
